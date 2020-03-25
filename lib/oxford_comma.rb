d89ae228138455a9fba041e5493d6a4ae062b0a7
Sonja Parsell <hello@sparsell.com>
2020-03-25 14:51:56 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  array_string = array
+  if array_string.length == 1
+    array_string = array.join(' ')
+  elsif
+    array_string.length == 2
+    array_string = array.join(' and ')
+  else
+    array_string.length > 3
+    array_string = array[0..-2].join(', ') + ", and " + array[-1]
+  end
+  return array_string
 end
