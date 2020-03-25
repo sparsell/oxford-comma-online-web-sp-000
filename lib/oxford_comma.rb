952c1eacc1f4a9491703563c8eefd316ba619036
Sonja Parsell <hello@sparsell.com>
2020-03-25 14:30:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,12 +7,12 @@ def oxford_comma(array)
   #array_string = array
   if array.length <= 1
     array.join(" ")
-  elsif
+  else
     array.length <= 2
     array.join(" and ")
-  else
-    array.length >= 3
-    array.join(" , ")
+  #else
+  #  array.length >= 3
+  #  array.join(" , ")
   end
   return array_string #+ array_last_item
 end
