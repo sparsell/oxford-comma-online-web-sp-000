f829c172e55cdd659617753ffc3602604321ce4b
Sonja Parsell <hello@sparsell.com>
2020-03-25 14:50:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,14 @@
-#def oxford_comma(array)
-#  array_string = array.join(" and ")
-#  return array_string
-#end
-
 def oxford_comma(array)
   array_string = array
   if array_string.length == 1
     array_string = array.join(' ')
   elsif
-    array_string.length == 2
+    array_string.length == 2 
     array_string = array.join(' and ')
   else
     array_string.length > 3
-    array_string.join(" and ")#, array_string.last.join(", and ")
-    #[array_string[0..-2].join(', '), array_string.last].join(', and ')
+    array_string = #array.join(" , "), array.last.join(", and ")
+    array[0..-2].join(', ') + " and " + array[-1]
   end
   return array_string
 end
