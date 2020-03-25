8106557d716323bc66f1d7eb8d459d3d476575de
Sonja Parsell <hello@sparsell.com>
2020-03-25 13:52:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,16 @@
+#def oxford_comma(array)
+#  array_string = array.join(" and ")
+#  return array_string
+#end
+
 def oxford_comma(array)
-  array_string = array.join(" and ")
-  return array_string
+  array_string = array
+  if array_string.length <= 1
+    array_string = array.join(" ")
+  elsif
+    array_string.length >= 2 
+    array_string = array.join(" and ")
+ 
+  end
+  return array_string #+ array_last_item
 end
