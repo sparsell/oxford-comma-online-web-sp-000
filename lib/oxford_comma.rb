d2d4a930ea6229e94054879dad3755269de19759
Sonja Parsell <hello@sparsell.com>
2020-03-25 13:52:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,9 +8,9 @@ def oxford_comma(array)
   if array_string.length <= 1
     array_string = array.join(" ")
   elsif
-    array_string.length >= 2 
+    array_string.length >= 2
     array_string = array.join(" and ")
- 
+
   end
   return array_string #+ array_last_item
 end
